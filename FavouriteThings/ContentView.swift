1f262f0d9cdfaaceecbfec9c353cab52ed1708f3
Noble-01 <zaccripps5@gmail.com>
2020-04-13 22:09:33 -0700

    Initial Commit

---
+++ FavouriteThings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouriteThings
+//
+//  Created by Zac Cripps on 13/4/20.
+//  Copyright © 2020 Zac Cripps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
