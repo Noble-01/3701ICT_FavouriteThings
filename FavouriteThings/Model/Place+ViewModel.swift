3c7ccee182f1136b34a1806718e3a1eb84a3b51e
Noble-01 <zaccripps5@gmail.com>
2020-05-05 20:41:53 -0700

    created getter and setter functions for long and lat vars in place+ViewModel

--- FavouriteThings/Model/Place+ViewModel.swift
+++ FavouriteThings/Model/Place+ViewModel.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import CoreLocation
+
+class Place: Identifiable, ObservableObject{
+    @Published var coordinates = CLLocationCoordinate2D(latitude: -27.962, longitude: 153.382)
+    
+    var name = ""
+    var latitude: String {
+        get { "\(coordinates.latitude)"}
+        set{
+            guard let coord = CLLocationDegrees(newValue)  else {return}
+            coordinates.latitude = coord
+        }
+    }
+    var longitude: String {
+        get {"\(coordinates.longitude)"}
+        set {
+            guard let coord = CLLocationDegrees(newValue)  else {return}
+            coordinates.longitude = coord
+        }
+    }
+}
