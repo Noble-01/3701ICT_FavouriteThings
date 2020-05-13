f6f9295c6d1f2db30f2f8b348d032aa0d74f9d96
Noble-01 <zaccripps5@gmail.com>
2020-05-12 18:00:45 -0700

    created file for thing delegate class

--- FavouriteThings/Model/ThingMapViewDelegate.swift
+++ FavouriteThings/Model/ThingMapViewDelegate.swift
@@ -1,49 +1,49 @@
 //
-//  Place+ViewModel.swift
+//  ThingMapViewDelegate.swift
 //  FavouriteThings
 //
-//  Created by Zac Cripps on 5/5/20.
+//  Created by Zac Cripps on 12/5/20.
 //  Copyright © 2020 Zac Cripps. All rights reserved.
 //
-
+import CoreData
+import SwiftUI
 import Foundation
-import CoreLocation
 import MapKit
-///create objective C class Place
-@objc class Place: NSObject, Identifiable, ObservableObject{
-    ///default coordinates for which the below vars use
-    ///when the var coordinates are changed they change the coordinates in the below variable which it then uses
-    @Published var coordinates = CLLocationCoordinate2D(latitude: -27.962, longitude: 153.382)
-    ///when a name is returned from a coordinated it is published and therefore can be seen in the location view
-    @Published var name = ""
+import CoreLocation
+
+class ThingMapViewDelegate: NSObject, Identifiable, ObservableObject{
     
-    var mapCoordinates = CLLocationCoordinate2D(latitude: -27.962, longitude: 153.382)
+    @ObservedObject  var thing: Thing
     
+    var textFieldLatitude: String
+    var textFieldLongitude: String
     
-    ///created varaible latitude  for map and stores in model
-    var latitude: String {
-        ///getter function for retrieving coordinates
-        get { "\(coordinates.latitude)"}
-        ///setter function to set the inital variable value
-        set{
-            guard let coord = CLLocationDegrees(newValue)  else {return}
-            coordinates.latitude = coord
-        }
+    init(thing: Thing) {
+        self.thing = thing
+        textFieldLatitude = thing.thingLatitude
+        textFieldLongitude = thing.thingLongitude
     }
-    ///created varaible longitude for map and stores in model
-    var longitude: String {
-        get {"\(coordinates.longitude)"}
-        set {
-            guard let coord = CLLocationDegrees(newValue)  else {return}
-            coordinates.longitude = coord
-        }
+    ///retrieve coordinates from model and place them into varaibles latitude and longitude
+    func getterMapCoordinates() -> CLLocationCoordinate2D{
+        return CLLocationCoordinate2D(latitude: thing.latitude, longitude: thing.longitude)
+    }
+    
+        ///set the long and lat values to the vars for the Thing object
+    func setterMapCoordinates(newCoordinates: CLLocationCoordinate2D){
+        thing.thingLongitude = "\(newCoordinates.longitude)"
+        thing.thingLatitude = "\(newCoordinates.latitude)"
+    }
+    ///set the long and lat values to the vars for the Thing object
+    func setterMapCoordinates(latitude: String, longitude: String){
+        thing.thingLongitude =  longitude
+        thing.thingLatitude = latitude
     }
     ///update coordinates once the name has been fully entered into the textfield
     func updateCoordinateFromName(){
             /// CLGeocoder allows for reverse and forwarding geocoding with both names and coordinates
             let geocoder = CLGeocoder()
             ///returns long and lat coordinates based on name used
-            geocoder.geocodeAddressString(name) { (maybePlaceMarks, maybeError) in
+        geocoder.geocodeAddressString(thing.thingLocationName) { (maybePlaceMarks, maybeError) in
                 ///location is optional in case we don't get anything returned
                 guard let placemark = maybePlaceMarks?.first,
                     ///location is an attribute of CLLocation class
@@ -59,17 +59,16 @@ import MapKit
                     return
                 }
                 ///update the coordinates which updates the view
-                self.coordinates = location.coordinate
-                self.updateMapCoordinate()
+                self.setterMapCoordinates(newCoordinates: location.coordinate)
             }
         }
     ///update name once the coordinates has been fully entered into the textfield
     func updateNameFromCoordinate(){
-        self.updateMapCoordinate()
+        setterMapCoordinates(latitude: textFieldLatitude, longitude: textFieldLongitude)
             /// CLGeocoder allows for reverse and forwarding geocoding with both names and coordinates
             let geocoder = CLGeocoder()
             ///var to store location name from coordinates
-            let location = CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
+        let location = CLLocation(latitude: thing.latitude, longitude: thing.longitude)
             ///returns location based on long and lat coordinates used
             geocoder.reverseGeocodeLocation(location) { (maybePlaceMarks, maybeError) in
                 ///location is optional in case we don't get anything returned
@@ -85,11 +84,19 @@ import MapKit
                 }
                 ///update name from placemark which is placed based on the coordinates or return the administrative area of the placemark etc
                 ///just returns something for user to see
-                self.name = placemark.name ?? placemark.administrativeArea ?? placemark.locality ?? placemark.subLocality ?? placemark.thoroughfare ?? placemark.subThoroughfare ?? placemark.country ?? "<Yeah your not on earth>"
+                self.thing.thingLocationName = placemark.name ?? placemark.administrativeArea ?? placemark.locality ?? placemark.subLocality ?? placemark.thoroughfare ?? placemark.subThoroughfare ?? placemark.country ?? "<Yeah your not on earth>"
             }
         }
-    func updateMapCoordinate(){
-        mapCoordinates = coordinates
-    }
+
 }
 
+///updates the lat and long coordinates from the coordinates taken at the centre of the map
+extension  ThingMapViewDelegate: MKMapViewDelegate{
+
+    ///updates coordinates once map has stopped moving
+   public func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        ///retrieves values from center of map
+        let centre = mapView.centerCoordinate
+        setterMapCoordinates(newCoordinates: centre)
+    }
+}
